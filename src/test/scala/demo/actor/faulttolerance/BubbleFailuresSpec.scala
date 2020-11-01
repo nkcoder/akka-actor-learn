0bb9981cb309c763ee43da8d53dc47af5e92ae0b
daniel <daniel5hbs@gmail.com>
2020-11-01 19:12:43 +0800

    remove unimplemented test case

--- src/test/scala/demo/actor/faulttolerance/BubbleFailuresSpec.scala
+++ src/test/scala/demo/actor/faulttolerance/BubbleFailuresSpec.scala
@@ -1,9 +1,7 @@
 package demo.actor.faulttolerance
 
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
-import akka.actor.typed.BehaviorInterceptor
-import akka.actor.typed.scaladsl.Behaviors
-import demo.actor.faulttolerance.BubbleFailuresDemo.Protocol.{Fail, Hello}
+import demo.actor.faulttolerance.BubbleFailuresDemo.Protocol.Hello
 import demo.actor.faulttolerance.BubbleFailuresDemo.Worker
 import org.scalatest.wordspec.AnyWordSpecLike
 
@@ -15,12 +13,6 @@ class BubbleFailuresSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike 
       val worker = testKit.spawn(Worker(), name = "worker-1")
       worker ! Hello("hello", probe.ref)
       probe.expectMessage(obj = "hello")
-    }
-
-    "restart when sending Fail to Worker" in {
-      val probe = testKit.createTestProbe[String]()
-      val worker = testKit.spawn(Worker(), name = "worker-2")
-      worker ! Fail("failed")
     }
   }
 
